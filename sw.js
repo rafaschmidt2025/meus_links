a0ec632278f2d7df247aa75e0c15fd105adbf41f
rafaschmidt2025 <unai.rafael@gmail.com>
2025-11-25 10:40:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "meus-links-v1";
+const ASSETS = [
+  "./",
+  "./meus_links.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((resp) => resp || fetch(event.request))
+  );
+});
